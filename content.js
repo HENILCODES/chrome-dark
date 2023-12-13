52105cf60a8c7f3b1b03bc2c34435d26c6e8e333
Henil Code ✨ <99518388+HENILCODES@users.noreply.github.com>
2023-12-13 11:00:32 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
 const style = document.createElement('style');
 style.textContent = `
-/* Your custom CSS code here */
 body , *{
   background-color: #0a0d10 !important;
   color:white !important;
 }
 input, textarea{
-    color:dark !important;
+    color:#cb3e3e !important;
+    border: 1px solid #2f4d4d !important;
 }
 `;
 
